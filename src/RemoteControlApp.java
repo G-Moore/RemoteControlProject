c6b2ef0a699579b18f1e70ff7c2442048c150dd5
George Moore <gdubya1218@gmail.com>
2019-11-08 16:51:07 -0700

    Added Scanner, Initial Message

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class RemoteControlApp {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("To begin, turn on the remote control. ");
 	}
 	// TODO add pressButton() method
 	// TODO add turnOn() method

d12b88a7855f9b8c537a2b87faf3b33fa50168d2
George Moore <gdubya1218@gmail.com>
2019-11-08 17:30:08 -0700

    Added TODO methods and power while loop

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -4,8 +4,22 @@ public class RemoteControlApp {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("To begin, turn on the remote control. ");
+		turnOn();
+		String power = sc.next();
+	
+	
+		while (!power.equals("ON")) {
+				turnOn();
+				power = sc.next();
+			} if (power.equals("ON")) {
+				turningOn();
+			}
 	}
+	public static void turnOn() {
+		System.out.print("To begin, turn on the remote control. ");
+	}
+	public static void turningOn() {
+		System.out.println("TURNING ON...");
 	// TODO add pressButton() method
-	// TODO add turnOn() method
+}
 }
